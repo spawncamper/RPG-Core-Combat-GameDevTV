98069874795ed7218b873a22d8296893cdfde62d
Mitch <ddiarikov@gmail.com>
2021-02-25 08:01:48 +0300

    CinematicTrigger.cs

---
+++ Assets/Scripts/CinematicTrigger.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        { GetComponent<PlayableDirector>().Play(); }
+    }
+}
