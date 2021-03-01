32ccb5440c657e191f931bb3de2919e88019e1b6
Mitch <ddiarikov@gmail.com>
2021-03-01 21:31:19 +0300

    Renamed folders, created portals, Portal.cs, DontDestroyOnLoad
    Coroutines

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(SceneTransition());
+        }
+    }
+
+    private IEnumerator SceneTransition()   // Transition()
+    {
+        yield return SceneManager.LoadSceneAsync(sceneName);
+        print("Sceneloaded");
+        Destroy(gameObject);
+    }
+}
